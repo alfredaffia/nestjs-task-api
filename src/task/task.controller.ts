7214b521aa940f8ca368743ddda8d339c446a4fc
alfredaffia <alfredaffia478@gmail.com>
2026-01-10 23:25:08 +0100

    added guards to task controller(protected routes)

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -3,40 +3,44 @@ import { TaskService } from './task.service';
 import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/guards/role.guards';
+import { Roles } from 'src/auth/decorators/role.decorator';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('tasks')
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+    constructor(private readonly taskService: TaskService) { }
 
-  @Post()
-  @ApiOperation({ summary: 'Create a new task' })
-  @ApiResponse({ status: 201, description: 'Task created' })
-  create(@Request() req, @Body() dto: CreateTaskDto){
-    return this.taskService.create(req.user.userId, dto);
-  }
+    @Post()
+    @UseGuards(RolesGuard)
+    @Roles('ADMIN')
+    @ApiOperation({ summary: 'Create a new task' })
+    @ApiResponse({ status: 201, description: 'Task created' })
+    create(@Request() req, @Body() dto: CreateTaskDto) {
+        return this.taskService.create(req.user.userId, dto);
+    }
 
-  @Get()
-  @ApiOperation({ summary: 'Get all my tasks' })
-  findAll(@Request() req) {
-    return this.taskService.findAll(req.user.userId);
-  }
+    @Get()
+    @ApiOperation({ summary: 'Get all my tasks' })
+    findAll(@Request() req) {
+        return this.taskService.findAll(req.user.userId);
+    }
 
-  @Get(':id')
-  @ApiOperation({ summary: 'Get a tasks by id' })
-  findOne(@Param('id') id: string, @Request() req) {
-    return this.taskService.findOne(+id, req.user.userId);
-  }
+    @Get(':id')
+    @ApiOperation({ summary: 'Get a tasks by id' })
+    findOne(@Param('id') id: string, @Request() req) {
+        return this.taskService.findOne(+id, req.user.userId);
+    }
 
-  @Patch(':id')
-  @ApiOperation({ summary: 'Update my tasks' })
-  update(@Param('id') id: string, @Request() req, @Body() dto: UpdateTaskDto) {
-    return this.taskService.update(+id, req.user.userId, dto);
-  }
+    @Patch(':id')
+    @ApiOperation({ summary: 'Update my tasks' })
+    update(@Param('id') id: string, @Request() req, @Body() dto: UpdateTaskDto) {
+        return this.taskService.update(+id, req.user.userId, dto);
+    }
 
-  @Delete(':id')
-  @ApiOperation({ summary: 'Delete my tasks' })
-  remove(@Param('id') id: string, @Request() req) {
-    return this.taskService.remove(+id, req.user.userId);
-  }
+    @Delete(':id')
+    @ApiOperation({ summary: 'Delete my tasks' })
+    remove(@Param('id') id: string, @Request() req) {
+        return this.taskService.remove(+id, req.user.userId);
+    }
 }
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,9 +4,10 @@ import { TaskController } from './task.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' }),AuthModule],
   providers: [TaskService,PrismaService,JwtStrategy],
   controllers: [TaskController]
 })
